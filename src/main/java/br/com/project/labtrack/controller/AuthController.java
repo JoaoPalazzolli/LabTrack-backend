76b274a437464af340d1b4995d5d4aa6345e2129
Joao Palazzolli <joaopedropalazzolli@gmail.com>
2025-05-11 00:28:25 -0300

    Removido codigo esquecido

--- src/main/java/br/com/project/labtrack/controller/AuthController.java
+++ src/main/java/br/com/project/labtrack/controller/AuthController.java
@@ -2,7 +2,6 @@ package br.com.project.labtrack.controller;
 
 import br.com.project.labtrack.dto.TokenDTO;
 import br.com.project.labtrack.dto.auth.UserAuthDTO;
-import br.com.project.labtrack.repository.PermissaoRepository;
 import br.com.project.labtrack.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,15 +12,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     @Autowired
-    private PermissaoRepository permissaoRepository;
-
-    @Autowired
     private AuthService authService;
-
-    @PostMapping(value = "/teste")
-    private void carregarPermissao(){
-
-    }
 
     @PostMapping(value = "/autenticar")
     public ResponseEntity<TokenDTO> autenticarUsuario(@RequestBody UserAuthDTO userAuthDTO){
